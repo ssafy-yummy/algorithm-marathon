70b29159d3584ca4ed2d920829f8963e4412ceb1
98pej11 <68580956+98pej11@users.noreply.github.com>
2022-08-21 16:24:04 +0900

    Update BOJ_13305_S4_주유소.java

--- eunjung/week3/BOJ_13305_S4_주유소.java
+++ eunjung/week3/BOJ_13305_S4_주유소.java
@@ -10,8 +10,8 @@ public class BOJ_13305_S4_주유소 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
-		road = new long[N-1];
-		city = new long[N];
+		road = new long[N-1]; // 도시
+		city = new long[N]; // 도로
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N-1; i++) {
 			road[i] = Integer.parseInt(st.nextToken());
@@ -25,11 +25,12 @@ public class BOJ_13305_S4_주유소 {
 	private static void calc() {
 		long sum;
 		int index;
-		sum = city[0]*road[0];
+		sum = city[0]*road[0]; // 처음엔 무조건 충전
 		index = 0;
 		for (int i = 1; i < N-1; i++) {
 			if(city[index]>city[i]) index = i;
 			sum += city[index]*road[i];
+			// 주유소 값 비교후 적은 주유소로 충전
 		}
 		System.out.println(sum);
 	}
