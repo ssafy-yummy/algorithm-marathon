d1a455f64c1d18495fe93d2929ec449d25633ee7
소지현 <s688922@gmail.com>
2022-08-28 16:52:26 +0900

    BOJ : 1915 G4 가장큰정사각형

--- jihyun/src/week4/BOJ_1915_G4_가장큰정사각형.java
+++ jihyun/src/week4/BOJ_1915_G4_가장큰정사각형.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_1915_G4_가장큰정사각형 {
+public class BOJ_1915_G4_가장큰정사각형 { 
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
