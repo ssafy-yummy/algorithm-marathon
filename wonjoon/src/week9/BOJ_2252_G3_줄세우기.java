93c3a8f85ee20a2e7791f19a5f32866d633586ed
swj <159632_@naver.com>
2022-10-01 21:23:07 +0900

    Fix : BOJ 2252 G3 줄 세우기 import 정리

--- wonjoon/src/week9/BOJ_2252_G3_줄세우기.java
+++ wonjoon/src/week9/BOJ_2252_G3_줄세우기.java
@@ -3,7 +3,6 @@ package week9;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
