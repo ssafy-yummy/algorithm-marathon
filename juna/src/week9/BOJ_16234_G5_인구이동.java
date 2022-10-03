dc0475c9ae0d4fe262dde35d1a8aa58560f4947a
wnsdk <jn307742@gmail.com>
2022-10-03 12:58:06 +0900

    Fix : BOJ 16234 G5 인구 이동, 주석 수정

--- juna/src/week9/BOJ_16234_G5_인구이동.java
+++ juna/src/week9/BOJ_16234_G5_인구이동.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BOJ_16234_G5_인구이동 {
+public class Main {
 	
 	static int N;	// N * N 크기의 땅
 	static int L;	// 인구 차이가 L명 이상
@@ -17,7 +17,8 @@ public class BOJ_16234_G5_인구이동 {
 	static int[] dx = new int[] {0, 0, 1, -1};
 	
 	public static void main(String[] args) throws Exception {
-
+		
+		// 1. 입력 받기
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
@@ -32,48 +33,37 @@ public class BOJ_16234_G5_인구이동 {
 				A[y][x] = Integer.parseInt(st.nextToken());
 		}
 		
+		
+		// 2. 문제 풀이
 		int day = 0;
 		while (true) {
 			
-			boolean idx = false;	// 인구 이동 발생 유무
+			// 인구 이동 발생 유무
+			boolean idx = false;	
 			visited = new boolean[N][N];
 			
 			for (int y = 0; y < N; y++)
 				for (int x = 0; x < N; x++)
 					if (!visited[y][x] && bfs(y, x))
-						idx = true;
+						// 인구 이동 발생!
+						idx = true;	
 			
 			// 인구 이동이 단 한번도 발생하지 않았다면
 			if (!idx) break;
 			
 			day++;
-
 		}
+		
+		
+		// 3. 정답 출력
 		System.out.println(day);
-	}
-
-	private static void print() {
-		for (int y = 0; y < N; y++) {
-			for (int x = 0; x < N; x++) 
-				System.out.print(A[y][x] + " ");
-			System.out.println();
-		}
-		System.out.println();
-	}
-	private static void print2() {
-		for (int y = 0; y < N; y++) {
-			for (int x = 0; x < N; x++) 
-				System.out.print(visited[y][x] + " ");
-			System.out.println();
-		}
-		System.out.println();
 	}
 
 	private static boolean bfs(int sy, int sx) {
 		
 		Queue<int[]> q = new LinkedList<>();
-		List<int[]> union = new LinkedList<>();
-		int total = 0;
+		List<int[]> union = new LinkedList<>();	// 연합국들의 좌표를 저장
+		int total = 0;	// 연합국들의 총 인구 수
 		
 		q.offer(new int[] {sy, sx});
 		union.add(new int[] {sy, sx});
@@ -90,7 +80,10 @@ public class BOJ_16234_G5_인구이동 {
 				int nx = x + dx[k];
 				
 				if (isCoord(ny, nx) && !visited[ny][nx]) {
+					
+					// 인접 국가간의 인구 수 차이
 					int diff = Math.abs(A[ny][nx] - A[y][x]);
+					
 					if (L <= diff && diff <= R) {
 						visited[ny][nx] = true;
 						q.offer(new int[] {ny, nx});
@@ -101,23 +94,31 @@ public class BOJ_16234_G5_인구이동 {
 			}
 		}
 		
+		// 인구 이동이 완료된 후, 한 국가가 갖게 될 인구 수
 		int population = total / union.size();
+		
+		// 인구 이동 전과 후의 인구 수 차이가 없다면
 		if (population == A[sy][sx]) {
+			
+			// 여태까지 방문했던 국가들의 방문 체크를 false로 되돌려주기
 			for (int[] i : union) {
 				int y = i[0];
 				int x = i[1];
-				if (y == 0 && x == 1) System.out.println("!!");
 				visited[y][x] = false;
 			}
+			
+			// 실질적인 인구 이동이 발생하지 않았다는 의미에서 false를 리턴
 			return false;
 		}
 		
+		// 새로운 인구 수를 연합국들에게 적용시켜주기
 		for (int[] i : union) {
 			int y = i[0];
 			int x = i[1];
 			A[y][x] = population;
 		}
 		
+		// 실질적인 인구 이동이 발생했다는 의미에서 true를 리턴
 		return true;
 	}
 
