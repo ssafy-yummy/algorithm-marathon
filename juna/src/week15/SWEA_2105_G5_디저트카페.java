5a5ce67af8c9480fcce3c8b57c8e56fa47d55063
wnsdk <jn307742@gmail.com>
2022-11-09 17:50:05 +0900

    SWEA : 2105 G5 디저트 카페

--- juna/src/week15/SWEA_2105_G5_디저트카페.java
+++ juna/src/week15/SWEA_2105_G5_디저트카페.java
@@ -2,16 +2,14 @@ package algorithm;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class SWEA_2105_G5_디저트카페 {
 	
 	static int N;
 	static int ans;
+	static int sy, sx;
+	static boolean[] chk;
 	static int[] dy = new int[] {1, 1, -1, -1};
 	static int[] dx = new int[] {1, -1, -1, 1};
 	
@@ -39,8 +37,12 @@ public class SWEA_2105_G5_디저트카페 {
 			// 2. 문제 풀이
 			ans = -1;
 			for (int y = 0; y < N; y++)
-				for (int x = 0; x < N; x++)
-					bfs(y, x);
+				for (int x = 0; x < N; x++) {
+					chk = new boolean[101];
+					sy = y;
+					sx = x;
+					dfs(y, x, 0, 0);
+				}
 
 			
 			// 3. 출력
@@ -50,49 +52,39 @@ public class SWEA_2105_G5_디저트카페 {
 		System.out.println(sb);
 	}
 	
-
 	
-	private static void bfs(int sy, int sx) {		
-		Queue<int[]> q = new LinkedList<>();
-		Queue<Set<Integer>> q2 = new LinkedList<>();
+	private static void dfs(int y, int x, int k, int cnt) {
 		
-		q.offer(new int[] {sy, sx, 0});	
-		q2.offer(new HashSet<>());		// set 전달용 큐
+		if (k == 4) return ;
 		
-		while (!q.isEmpty()) {
-			int[] cur = q.poll();
-			int y = cur[0];
-			int x = cur[1];
-			int k = cur[2];		// 현재 이동 방향
-			Set<Integer> set = q2.poll();	// 여태까지 먹은 디저트
-			
-			// 한 바퀴 돌아서 처음 위치에 도착
-			if (y == sy && x == sx && k != 0) {
-				ans = Math.max(ans, set.size());
-				continue;
-			}
-			
-			if (k == 4) continue;
-			
-			int ny = y + dy[k];
-			int nx = x + dx[k];
-			
-			if (!isCoord(ny, nx)) continue;	// 맵을 벗어나면 안 됨
-			if (set.contains(map[ny][nx])) continue;	// 이미 먹은 디저트면 안 됨
-			
-			Set<Integer> nset = new HashSet<>();
-			nset.addAll(set);
-			nset.add(map[ny][nx]);
-			
-			// 진행하던 방향으로 그대로 진행하기
-			q.offer(new int[] {ny, nx, k});
-			q2.offer(nset);
-			
-			// 방향 꺾기
-			if (k == 2 && ny + nx != sy + sx) continue;	// 마지막으로 방향을 꺾을 때는 백트래킹 적용
-			q.offer(new int[] {ny, nx, k + 1});
-			q2.offer(nset);
+		if (y == sy && x == sx && cnt != 0) {
+			ans = Math.max(ans, cnt);
+			return;
 		}
+		
+		int ny = y + dy[k];
+		int nx = x + dx[k];
+		
+		if (!isCoord(ny, nx)) return;
+		if (chk[map[ny][nx]]) return;
+		
+		// map[ny][nx] 디저트를 먹었음을 표시
+		chk[map[ny][nx]] = true;
+		
+		// 진행 방향 그대로 이동하기
+		dfs(ny, nx, k, cnt + 1);
+		
+		// 방향 꺾기
+		// 마지막 꺾기 때에는 처음 좌표와 꺾는 좌표가 같은 대각선에 있어야 됨 (백트래킹)
+		if (k == 2 && ny + nx != sy + sx) {
+			chk[map[ny][nx]] = false;
+			return; 
+		}
+		
+		dfs(ny, nx, k + 1, cnt + 1);
+		
+		// chk 배열 원상 복귀
+		chk[map[ny][nx]] = false;
 	}
 
 	
